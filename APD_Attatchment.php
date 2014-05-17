795b334320897a651847be2c809493a29ff23401
MicNeo <MicNeo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-17 10:48:59 +0000

    * Fix: Fix for options page
    git-svn-id: https://plugins.svn.wordpress.org/auto-post-download/tags/1.3.2@916068 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ APD_Attatchment.php
@@ -0,0 +1,120 @@
+<?php
+
+class APD_Attatchment {
+
+	private $postId;
+	private $post;
+	private $image;
+	private $uploadDir;
+	private $zipFile;
+	private $zipArchive;
+	private $options;
+
+	function __construct($postId){
+		$this->postId = $postId;
+		$this->post = get_post($this->postId);
+		$this->image = wp_get_attachment_metadata(get_post_thumbnail_id($this->postId));
+		$this->uploadDir = wp_upload_dir();
+		$this->zipFile = $this->uploadDir['path']."/{$this->postId}.zip";
+		$this->zipArchive  = new ZipArchive();
+		$this->options = get_option('apd_options');
+		add_action('publish_post', 'apd_generate_attachment' );
+	}
+
+	public function generate(){
+		$cats = $this->options['apd_cats'];
+
+		if($cats == null || in_category( $cats, $this->postId)){
+			$this->generateZipFile();
+			$this->generateAttachment();
+		}
+	}
+
+	private function generateZipFile(){
+		if (!$this->zipArchive->open($this->zipFile, ZIPARCHIVE::OVERWRITE)){
+			die("Failed to create archive\n");
+		}
+
+		// do we have image attached to this post?
+		if(is_array($this->image) && $this->getItemsIncluded() != 'text'){
+			$filetype = wp_check_filetype(basename($this->image['file']));
+			$this->zipArchive->addFile(WP_CONTENT_DIR . "/uploads/" . $this->image['file'], "image." . $filetype['ext']);
+		}
+
+		if($this->getItemsIncluded() != 'image'){
+			if($this->options['apd_format'] == null || $this->options['apd_format'] == 'html'){
+				$this->zipArchive->addFromString("content.html", $this->generateHTMLContent());
+			} else {
+				$this->zipArchive->addFromString("content.txt", $this->generatePlainContent());
+			}
+		}
+		
+		if (!$zipArchive->status == ZIPARCHIVE::ER_OK)
+			echo "Failed to write local files to zip\n";
+
+		$this->zipArchive->close();
+	}
+
+	private function generateHTMLContent(){
+		$content = '<!DOCTYPE html><html lang="en"><head><meta charset=utf-8 /><title></title></head><body>';
+
+		$meta_list = $this->options['apd_meta'];
+
+		foreach($meta_list as $meta){
+			$content .= get_post_meta($this->postId, $meta, true);
+		}
+
+		$content .= $this->post->post_content . '</body></html>';
+
+		return $content;
+	}
+
+	private function generatePlainContent(){
+		$content = null;
+
+		$meta_list = $this->options['apd_meta'];
+
+		foreach($meta_list as $meta){
+			$content .= wp_strip_all_tags(get_post_meta($this->postId, $meta, true));
+		}
+
+		$content .= wp_strip_all_tags($this->post->post_content);
+
+		return $content;
+	}
+
+	private function generateAttachment(){
+		if(get_post_meta($this->postId, 'adp_attachment_id', true) == null){
+			require_once( ABSPATH . 'wp-admin/includes/image.php' );
+			$filetype = wp_check_filetype(basename($this->zipFile), null);
+			$attachment = array(
+					'guid' => $upload_dir['url'] . '/' . basename( $zipFile ),
+					'post_mime_type' => $filetype['type'],
+					'post_title' => preg_replace( '/\.[^.]+$/', '', basename( $zipFile ) ),
+					'post_content' => '',
+					'post_status' => 'inherit'
+			);
+			$attachId = wp_insert_attachment($attachment, $this->zipFile, $this->postId);
+			$attachData = wp_generate_attachment_metadata($attachId, $this->zipFile );
+			wp_update_attachment_metadata( $attachId, $attachData );
+
+			add_post_meta($this->postId, 'adp_attachment_id', $attachId);
+		}
+	}
+
+	private function getItemsIncluded(){
+		$type = $this->options['apd_content'];
+
+		switch($type){
+			case 'image':
+				return 'image'; break;
+			case 'text':
+				return 'text'; break;
+			default:
+				return 'both'; break;
+		}
+	}
+
+}
+
+?>
---
+++ APD_Settings.php
@@ -0,0 +1,229 @@
+<?php
+class APD_Settings
+{
+	/**
+	 * Holds the values to be used in the fields callbacks
+	 */
+	private $options;
+	private $selected_cats;
+	private $selected_custom_fields;
+	
+	/**
+	 * Start up
+	 */
+	public function __construct()
+	{
+		add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
+		add_action( 'admin_init', array( $this, 'page_init' ) );
+		// Set class property
+		$this->options = get_option( 'apd_options' );
+		$this->selected_cats = (($this->options['apd_cats'] == null) ? array() : $this->options['apd_cats']);
+		$this->selected_custom_fields = (($this->options['apd_meta'] == null) ? array() : $this->options['apd_meta']);
+	}
+
+	/**
+	 * Add options page
+	 */
+	public function add_plugin_page()
+	{
+		add_options_page(
+		'Auto Post Download',
+		'Auto Post Download',
+		'manage_options',
+		'apd_admin',
+		array( $this, 'create_admin_page' )
+		);
+	}
+
+	/**
+	 * Options page callback
+	 */
+	public function create_admin_page()
+	{
+		
+		?>
+<div class="wrap">
+	<h2>
+		<?php echo __( 'Auto Post Download', 'apd_auto-post-download' ); ?>
+	</h2>
+	<form method="post" action="options.php">
+		<?php
+		// This prints out all hidden setting fields
+		settings_fields( 'apd_settings_group' );
+		do_settings_sections( 'apd_settings_admin' );
+		submit_button();
+		?>
+	</form>
+</div>
+<?php
+	}
+
+	/**
+	 * Register and add settings
+	 */
+	public function page_init()
+	{
+		register_setting(
+		'apd_settings_group', // Option group
+		'apd_options', // Option name
+		array( $this, 'sanitize' ) // Sanitize
+		);
+		
+		add_settings_section(
+		'setting_format_section_id', // ID
+		__( 'Auto Post Download Settings', 'apd_auto-post-download' ) , // Title
+		array( $this, 'print_format_section_info' ), // Callback
+		'apd_settings_admin' // Page
+		);
+		
+		add_settings_section(
+		'setting_content_section_id', // ID
+		__( 'Auto Post Download Settings', 'apd_auto-post-download' ) , // Title
+		array( $this, 'print_content_section_info' ), // Callback
+		'apd_settings_admin' // Page
+		);
+
+		add_settings_section(
+		'setting_cats_section_id', // ID
+		__( 'Categories', 'apd_auto-post-download' ) , // Title
+		array( $this, 'print_cats_section_info' ), // Callback
+		'apd_settings_admin' // Page
+		);
+		
+		add_settings_section(
+		'setting_custom_fields_section_id', // ID
+		__('Custom fields', 'apd_auto-post-download' ), // Title
+		array( $this, 'print_custom_fields_section_info' ), // Callback
+		'apd_settings_admin' // Page
+		);
+
+		add_settings_field(
+		'apd_format', // ID
+		__("Format: ", 'apd_auto-post-download' ), // Title
+		array( $this, 'format_callback' ), // Callback
+		'apd_settings_admin', // Page
+		'setting_format_section_id' // Section
+		);
+		
+		add_settings_field(
+		'apd_format', // ID
+		__("Format: ", 'apd_auto-post-download' ), // Title
+		array( $this, 'content_callback' ), // Callback
+		'apd_settings_admin', // Page
+		'setting_content_section_id' // Section
+		);
+		
+		add_settings_field(
+		'apd_cats', // ID
+		__("Categories: ", 'apd_auto-post-download' ), // Title
+		array( $this, 'cats_array_callback' ), // Callback
+		'apd_settings_admin', // Page
+		'setting_cats_section_id' // Section
+		);
+
+		add_settings_field(
+		'apd_meta', // ID
+		__("Custom fields: ", 'apd_auto-post-download' ), // Title
+		array( $this, 'meta_array_callback' ), // Callback
+		'apd_settings_admin', // Page
+		'setting_custom_fields_section_id' // Section
+		);
+	}
+
+	/**
+	 * Sanitize each setting field as needed
+	 *
+	 * @param array $input Contains all settings fields as array keys
+	 */
+	public function sanitize( $input )
+	{
+		return $input;
+	}
+
+	public	function print_cats_section_info(){
+		echo __("Choose categories of posts for which post pack should be generated.", 'apd_auto-post-download');
+	}
+	
+	public	function print_format_section_info(){
+		echo __("Choose in which format content of post should be generated, by default: html.", 'apd_auto-post-download');
+	}
+	
+	public	function print_content_section_info(){
+		echo __("Choose what should be included in generated archive (post content or it's image), by default: both.", 'apd_auto-post-download');
+	}
+	
+	public	function print_custom_fields_section_info(){
+		echo __("Choose custom fields which should be included in content of generated attachment.", 'apd_auto-post-download');
+	}
+
+	public function cats_array_callback()
+	{
+		?>
+<select size="20" name="apd_options[apd_cats][]" multiple="multiple">
+	<?php 
+	$categories = get_categories();
+	
+	foreach ($categories as $category) {
+			  		$option = '<option value="' .$category->term_id . '" ' . ((in_array($category->term_id, $this->selected_cats)) ? 'selected="selected"' : '') . '>';
+			  		$option .= $category->cat_name;
+			  		$option .= ' ('.$category->category_count.')';
+			 	 	$option .= '</option>';
+			  		echo $option;
+				}
+				?>
+</select>
+<?php 
+	}
+
+	public function meta_array_callback()
+	{
+		?>
+<select size="20" name="apd_options[apd_meta][]" multiple="multiple">
+	<?php 
+	$list_of_meta = $this->get_all_meta();
+	
+	foreach ($list_of_meta as $meta) {
+		$meta = $meta[0];
+		$option = '<option value="' . $meta . '" ' . ((in_array($meta, $this->selected_custom_fields)) ? 'selected="selected"' : '') . '>';
+		$option .= $meta;
+		$option .= '</option>';
+		echo $option;
+	}
+	?>
+</select>
+<?php 
+	}
+
+	private function get_all_meta(){
+		global $wpdb;
+		$data = $wpdb->get_results("SELECT meta_key FROM $wpdb->postmeta group by meta_key order by meta_key", ARRAY_N);
+		return $data;
+	}
+	
+public function format_callback()
+	{
+		$format = $this->options['apd_format'];		
+		?>
+			<label for="html_format"><?php _e('HTML', 'apd_auto-post-download' ); ?></label>
+			<input id="html_format" type="radio" name="apd_options[apd_format]" value="html" <?php echo ($format == "html" || $format==null) ? 'checked="checked"' : ""; ?> />
+			<label for="plain_format"><?php _e('Plain text', 'apd_auto-post-download' ); ?></label>
+			<input id="plain_format" type="radio" name="apd_options[apd_format]" value="text" <?php echo ($format == "text") ? 'checked="checked"' : ""; ?> />
+		<?php 
+	}
+	
+	public function content_callback()
+	{
+		$content = $this->options['apd_content'];
+		?>
+			<label for="both_content"><?php _e('Both', 'apd_auto-post-download' ); ?></label>
+			<input id="both_content" type="radio" name="apd_options[apd_content]" value="both" <?php echo ($content == "both" || $content==null) ? 'checked="checked"' : ""; ?> />
+			<label for="text_content"><?php _e('Content only', 'apd_auto-post-download' ); ?></label>
+			<input id="text_content" type="radio" name="apd_options[apd_content]" value="text" <?php echo ($content == "text") ? 'checked="checked"' : ""; ?> />
+			<label for="image_format"><?php _e('Image only', 'apd_auto-post-download' ); ?></label>
+			<input id="image_format" type="radio" name="apd_options[apd_content]" value="image" <?php echo ($content == "image") ? 'checked="checked"' : ""; ?> />
+			<?php 
+		}
+	
+}
+
+?>
